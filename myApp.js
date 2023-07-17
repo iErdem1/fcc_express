cea42cce9aec2fb189fdafa06bc96e61b861e57a
IHSAN ERDEM <ihsanerdem@IHSANs-Air.fios-router.home>
2023-07-17 17:49:21 -0400

    /json route added

--- myApp.js
+++ myApp.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
     res.sendFile(absolute_path);
 });
 
+app.get("/json", (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 
