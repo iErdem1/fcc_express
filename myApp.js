6018f3fbe16b8a472d5574cb17bec3bd2e63e52b
IHSAN ERDEM <ihsanerdem@ihsans-air.mynetworksettings.com>
2023-07-18 00:13:51 -0400

    query params

--- myApp.js
+++ myApp.js
@@ -46,6 +46,12 @@ app.get("/:word/echo", (req,res) => {
     }); 
 });
 
+app.get("/name", (req, res) => {
+    var {first: firstName, last = lastName} = req.query;
+    res.json({
+        name: `${firstName} ${lastName}`
+    });
+});
 
 
 
